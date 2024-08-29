69dda4dd8b9a67b4914fd45a8cb4bc462eed1a85
bilge <cengizhan1457@gmail.com>
2024-08-29 17:11:35 +0300

    rke2python with host defined

--- myrke2script.py
+++ myrke2script.py
@@ -1,13 +1,20 @@
+import time
 import myparamiko
 import threading
 from datetime import datetime
 from scp import SCPClient
-def backup(router):
-    # Make a copy of the router dictionary and remove the 'hostname' key
-    router_copy = router.copy()
-    hostname = router_copy.pop('hostname', None)  # Remove 'hostname' key
+router1 = {'server_ip': '10.0.0.20', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke21'}
+router2 = {'server_ip': '10.0.0.21', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke22'}
+router3 = {'server_ip': '10.0.0.22', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke23'}
 
+routers = [router1, router2,  router3]
+# Define your domain
+domain = "example.com"
+myparamiko.generate_hosts_file(routers,domain)
+def backup(router):
     try:
+        router_copy = router.copy()
+        hostname = router_copy.pop('hostname', None)  # Remove 'hostname' key
         client = myparamiko.connect(**router_copy)  # Now it won't have the 'hostname' key
         shell = myparamiko.get_shell(client)
 
@@ -18,13 +25,13 @@ def backup(router):
         # Check NTP status
         cmd = 'if [[ $(timedatectl status | grep -i "NTP service" | awk \'{print $3}\') == "active" ]]; then echo "NTP is running"; else echo "NTP is not running"; fi'
         myparamiko.send_command(shell, cmd)
-
         # Set hostname using the extracted value
         if hostname:
             hostname_cmd = f'hostnamectl set-hostname {hostname}'
             myparamiko.send_command(shell, hostname_cmd)
-
+        myparamiko.scpfile(client,'hosts','/tmp/hosts')
         # Additional commands and operations
+        myparamiko.send_command(shell,'mv -f /tmp/hosts /etc/hosts')
         output = myparamiko.show(shell)
         print(f"Output from {router_copy['server_ip']}:\n{output}")
         output_list = output.splitlines()
@@ -38,7 +45,7 @@ def backup(router):
         mounth = now.month
         day = now.day
 
-        file_name = f'{router["server_ip"]}_{year}-{mounth}-{day}.txt'
+        file_name = f'{router_copy["server_ip"]}_{year}-{mounth}-{day}.txt'
         print(file_name)
         if 'NTP is running' in output:
             print("NTP is running as the expected.")
@@ -49,18 +56,13 @@ def backup(router):
 
         with open(file_name, 'w') as f:
             f.write(output)
-        myparamiko.close(client)
+
 
     except Exception as e:
         print(f"Error occurred on {router_copy['server_ip']}: {e}")
 
-
-
-router1 = {'server_ip': '10.0.0.20', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke21'}
-router2 = {'server_ip': '10.0.0.21', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke22'}
-router3 = {'server_ip': '10.0.0.22', 'server_port': '22', 'user':'bilge', 'passwd':'123', 'hostname': 'rke23'}
-
-routers = [router1, router2,  router3]
+    finally:
+        myparamiko.close(client)
 threads = list()
 for router in routers:
     th = threading.Thread(target=backup, args=(router,))
